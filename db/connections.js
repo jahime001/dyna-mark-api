b7d22fc0ca6252b6d58709115985889c3a6860e7
mobrewer <morgiearan@gmail.com>
2022-12-08 13:23:27 -0700

    Added .env file for DB, and got seed data to pull

--- db/connections.js
+++ db/connections.js
@@ -1,8 +1,20 @@
 //connection
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/dyna-mark', { useNewUrlParser: true })
-
 mongoose.Promise = Promise
+
+let mongoURI = ""
+if (process.env.NODE_ENV === "production") {
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://127.0.0.1:27017/dyna-mark";
+}
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((instance) =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch((error) => console.log('Connection failed!', error));
 
 module.exports = mongoose
