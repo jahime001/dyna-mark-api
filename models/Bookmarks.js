53e23ab06ed6c5b87ef33c6b6da2128216bce4d3
Bibhor2000 <daxter740@gmail.com>
2022-12-08 10:13:51 -0500

    created base for seed, connections, and model files

--- models/Bookmarks.js
+++ models/Bookmarks.js
@@ -1,1 +1,18 @@
 //bookmarks
+const mongoose = require('../db/connections')
+const Schema = mongoose.Schema
+
+const Tags = new Schema({
+    desc: String
+})
+
+const BookMark = new Schema({
+  title: String,
+  description: String,
+  link: String,
+  tags: [Tags],
+  likes: Boolean,
+  dislikes: Boolean
+})
+
+module.exports = mongoose.model('BookMark', BookMark)
