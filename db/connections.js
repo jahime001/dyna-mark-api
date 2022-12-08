7c5c09ebbf7a9e6b0abb9ab692e42b07b32f050f
mobrewer <morgiearan@gmail.com>
2022-12-08 13:48:29 -0700

    Updated connections page, tested routes, Everything working

--- db/connections.js
+++ db/connections.js
@@ -1,20 +1,14 @@
 //connection
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.Promise = Promise
-
-let mongoURI = ""
-if (process.env.NODE_ENV === "production") {
-  mongoURI = process.env.DB_URL;
-} else {
-  mongoURI = "mongodb://127.0.0.1:27017/dyna-mark";
-}
+const mongoURI = process.env.DB_URL
 
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((instance) =>
-    console.log(`Connected to db: ${instance.connections[0].name}`)
-  )
-  .catch((error) => console.log('Connection failed!', error));
+    .connect(mongoURI)
+    .then((instance) =>
+        console.log(`Connected to db: ${instance.connections[0].name}`)
+    )
+    .catch((error) => console.log('Connection failed!', error));
 
-module.exports = mongoose
+module.exports = mongoose;
