27368de4077c31c09fc72bbb54b2a45be07162e3
mobrewer <morgiearan@gmail.com>
2022-12-08 08:41:45 -0700

    Added routes to controllers

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,1 +1,66 @@
 //bookmarkController
+
+const express = require('express')
+const router = express.Router()
+
+const BookMark = require('../models/Bookmarks')
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const bookmarks = await BookMark.find({})
+        res.json(bookmarks)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const bookmark = await BookMark.findById(req.params.id)
+        res.json(bookmark)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newBookmark = await BookMark.create(req.body)
+        res.status(201).json(newBookmark)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const updateBookmark = await BookMark.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true}
+        )
+        if(updateBookmark) {
+            res.json(updateBookmark)
+        } else {
+            res.sendStatus(404)
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deleteBookmark = await BookMark.findByIdAndDelete(req.params.id)
+        if(deleteBookmark) {
+            res.sendStatus(204)
+        } else {
+            res.sendStatus(404)
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
